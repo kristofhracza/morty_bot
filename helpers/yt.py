ced6b6abea22d05194fc100049b38586a4c8b630
IVBecy <kristofhracza@gmail.com>
2023-05-24 23:52:15 +0100

    Fixed: audio stopage

--- helpers/yt.py
+++ helpers/yt.py
@@ -31,8 +31,10 @@ ytdl_format_options = {
     'source_address': '0.0.0.0'
 }
 
+# Fix for music stopage: https://stackoverflow.com/questions/66070749/how-to-fix-discord-music-bot-that-stops-playing-before-the-song-is-actually-over
 ffmpeg_options = {
-    'options': '-vn'
+    'options': '-vn',
+    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
 }
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
